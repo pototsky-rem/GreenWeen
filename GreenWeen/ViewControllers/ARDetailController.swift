ffe001e2e86e8af99df6d5f5c2f3d9858366327e
rem <pototsky.rem@gmail.com>
2021-06-19 16:21:03 +0300

    Update ARDetailController.swift
    add print method

--- GreenWeen/ViewControllers/ARDetailController.swift
+++ GreenWeen/ViewControllers/ARDetailController.swift
@@ -16,6 +16,7 @@ class ARDetailController: UITableViewController {
         setupStrechyHeaderView()
         setUpModels()
         setupTableViewRegisters()
+        print("Hello Moto")
     }
     
     private func setUpModels() {
